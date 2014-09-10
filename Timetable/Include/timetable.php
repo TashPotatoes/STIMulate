11fde7dabce939f325a9107f00954925d89e82ef
crazygravy89 <crazygravy89@hotmail.com>
2014-09-10 20:51:29 +1000

    Added absenties to timetable.

--- Timetable/Include/timetable.php
+++ Timetable/Include/timetable.php
@@ -14,11 +14,16 @@
     require_once '/php/SqlObject.php';
     //amazonPush();
     //amazonPull();
-$lq = "SELECT stu_name_first, stu_name_last, shift_id, shi_day, shi_time, str_shortname, str_name FROM STIMulate.facilitators JOIN STIMulate.shifts ON facilitators.student_id=shifts.student_id JOIN STIMulate.streams ON shifts.shi_stream=streams.stream_id WHERE facilitators.active = 1 ORDER BY shift_id asc";
+$lq = "SELECT facilitators.student_id, stu_name_first, stu_name_last, shift_id, shi_day, shi_time, str_shortname, str_name
+        FROM STIMulate.facilitators
+        JOIN STIMulate.shifts ON facilitators.student_id=shifts.student_id
+        JOIN STIMulate.streams ON shifts.shi_stream=streams.stream_id
+        WHERE facilitators.active = 1
+        ORDER BY shift_id asc;";
     $sqlObject = new \php\SqlObject($lq); // WHERE shifts.student_id = :student_id", array(8571091)); // HARD CODED
     $shiftInformation = $sqlObject->Execute();
-    $sqlObject = new \PHP\SqlObject("SELECT * FROM STIMulate.volunteerabsenties WHERE volunteerabsenties.volunteerID = :volunteerID", array(8276617)); // HARD CODED
-    $absentInformation = $sqlObject->Execute();
+
+    $absentInformation = RetrieveAbsentRecords();
 
     $DAYS_IN_WEEK = 5;
     $START_HOUR = '08:00:00'; // Must be before 12 noon, whole hour only (0-12), set to hour before earliest shift. // CHANGE THIS TO BE A QUERY
@@ -40,14 +45,28 @@ $lq = "SELECT stu_name_first, stu_name_last, shift_id, shi_day, shi_time, str_sh
     }
 
     function InsertActivity($shiftInformation, $absentInformation, $day, $currentTime){
-        $dayArray = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday');
-        $currentDayWords = $dayArray[$day];
-        $returned = false;
         for($i = 0; $i < count($shiftInformation); $i++){
             if($shiftInformation[$i]['shi_day'] == $day) {
                 if($currentTime == $shiftInformation[$i]['shi_time'])
                 {
-                    NameCard($shiftInformation[$i], $absentInformation);
+                    if(count($absentInformation) > 0) {
+                        for($j = 0; $j < count($absentInformation); $j++){
+                            $absentDay = date("w", strtotime($absentInformation[$j]["absence_timestamp"]))-2;
+                            $absentTime = date("H:i", strtotime($absentInformation[$j]["absence_timestamp"]));
+
+                            if($shiftInformation[$i]["shi_time"] == $absentTime && $day == $absentDay &&
+                                $absentInformation[$j]["volunteer_id"] == $shiftInformation[$i]["student_id"]){
+
+                            } else {
+                                NameCard($shiftInformation[$i], $absentInformation);
+                                break;
+                            }
+                        }
+                    } else {
+                        NameCard($shiftInformation[$i], $absentInformation);
+                    }
+
+
                 }
                 //$hours = explode('.', $shiftInformation[$i]['duration'])[0];
                 //$seconds = explode('.', $shiftInformation[$i]['duration'])[1];
@@ -69,6 +88,27 @@ $lq = "SELECT stu_name_first, stu_name_last, shift_id, shi_day, shi_time, str_sh
         echo "</span>";
 
 
+    }
+
+    function RetrieveAbsentRecords(){
+        $sqlObject = new \PHP\SqlObject("SELECT * FROM STIMulate.absences WHERE active = 1;");
+        $absentInformation = $sqlObject->Execute();
+
+        $dateNow = date("d", strtotime("now"));
+        for($i = 0; $i < count($absentInformation); $i++) {
+            $absentDate =  date("d", strtotime($absentInformation[$i]["absence_timestamp"]));
+
+            if($dateNow + 7 < $absentDate) {
+                DisableAbsentRecord($absentInformation[$i]["absence_id"]);
+            }
+        }
+
+        return $sqlObject->Execute();
+    }
+
+    function DisableAbsentRecord($absentID){
+        $sqlObject = new \PHP\SqlObject("UPDATE STIMulate.absences SET active = 0 WHERE absence_id = :absence_id;", array($absentID));
+        $sqlObject->Execute();
     }
 
 ?>
--- Timetable/Index.php
+++ Timetable/Index.php
@@ -2,10 +2,10 @@
         <div class="timetable-filter">
             <div class="filter-stream">
                 <ul>
-                    <li id="filterStreamIt"class="stream-select IT">IT</li>
-                    <li id="filterStreamSc"class="stream-select SC">Science</li>
-                    <li id="filterStreamMa"class="stream-select MA">Maths</li>
-                    <li id="filterStreamDH"class="stream-select DH">Duty Hosts</li>
+                    <li id="filterStreamIt" class="stream-select IT">IT</li>
+                    <li id="filterStreamSc" class="stream-select SC">Science</li>
+                    <li id="filterStreamMa" class="stream-select MA">Maths</li>
+                    <li id="filterStreamDH" class="stream-select DH">Duty Hosts</li>
                 </ul>
                 <span class="clearfix"></span>
             </div>
