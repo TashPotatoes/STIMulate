4e17b254e12c084620a5f8c49cac028ba1a96841
Natasha <natasha.bethany.moffat@gmail.com>
2014-09-13 15:04:57 -0700

    pearls stuff

---
+++ archived automatic/Admin.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+	<h1> Admin page </h1>
+	<p> Initiate the algorithm only after cut off dates. Minor changes to students 
+	preferred time slots can result in an entirely new time sheet</p>
+	
+	<form formaction = "Simplex.php">
+		<button>Compute PLF Time sheets</button>
+	</form>
+
+
+</body>
+</html>
---
+++ archived automatic/PLF.php
@@ -0,0 +1,6 @@
+
+<?php
+    require_once "matrix.php";
+
+    $matrix = new matrix()
+?>
