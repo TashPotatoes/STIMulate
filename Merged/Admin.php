f5489c61bf97c95e53fe79b8a10461f4805f21a4
crazygravy89 <crazygravy89@hotmail.com>
2014-09-25 21:57:14 +1000

    admin begins

---
+++ Merged/Admin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hayden
+ * Date: 16/09/14
+ * Time: 7:09 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include 'Include/GlobalHead.inc'; ?>
+    <link href="CSS/SideBar.CSS" rel="stylesheet" type="text/css">
+    <link href="CSS/LocationSeparator.CSS" rel="stylesheet" type="text/css">
+</head>
+<body>
+<main>
+    <?php include 'Include/Header.inc'; ?>
+    <?php
+    $currentPlace = "<a href = \"index.php\">
+                    <img src = \"IMG/dashboard.png\" alt = \"dashboard\" class = \"inline-image\">
+                        <p>Home</p>
+                    </a>
+                    <p> > Admin</p>";
+    include 'Include/LocationSeparator.inc'; ?>
+    <?php include 'Include/SideBar.inc'; ?>
+    <?php include 'Include/Admin.inc'; ?>
+
+</main>
+</body>
+</html>
