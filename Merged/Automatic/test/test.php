73147768cd737e6ddbbd20a25489ec678fcdeb9b
Natasha <natasha.bethany.moffat@gmail.com>
2014-10-07 23:28:42 +1000

    fixed some errors but $.ajax call still causing an error

--- Merged/Automatic/test/test.php
+++ Merged/Automatic/test/test.php
@@ -1,8 +1,15 @@
+
 <!DOCTYPE html>
 <html>
 <head>
+<?php include '../../Include/GlobalHead.inc'; ?>
+<link href="../../CSS/SideBar.CSS" rel="stylesheet" type="text/css">
+<link href="../../CSS/LocationSeparator.CSS" rel="stylesheet" type="text/css">
+<link href="../../CSS/TimeTableFilter.CSS" rel="stylesheet" type="text/css">
+<link href="../../CSS/ManagePfl.CSS" rel="stylesheet" type="text/css">
 	<meta charset="UTF-8">
 	<script src="../dist/glpk.min.js"></script>	
+	
 </head>
 <body>
 <?php
@@ -11,132 +18,180 @@
     require_once '../../php/SqlObject.php';
     require '../../php/uac.php';
 	
+	$studentArray;
+	$stream = "";
+	
+	function generateCPLEX($stream){
+			// Generate array of studentsID, Index of studentID in array will be used to generate the input CPLEX string
+			$studentListRS = new \PHP\SqlObject("SELECT * FROM preferences ORDER BY `user_id` ASC WHERE  `stream` = :stream; GROUP BY `user_id`", array(strtoupper($stream))); 
+			$studentListRS->Execute();
+			$studentArray = array();
+			$studentTotal = 0;
+			foreach($studentListRS as $row) {
+				$studentArray[$studentTotal] = $row['student_ID'];
+				$studentTotal++;
+			}
+			if ($studentTotal == 0){
+				echo "no input data in database";
+			}
+			else if ($studentTotal < 8*5){
+				echo "you have " . $studentTotal . " it is recommended that you wait for more students to submit preferences";
+			}
+			
+			$studentHours = array(); //TODONE create hours in db and then uncomment code below and edit for loop for generating relevant constraints
+			$studentHoursRS = new \PHP\SqlObject("SELECT `student_id`, hours_" . $stream . "FROM facilitators JOIN preferences on facilitators.student_id = preferences.user_id WHERE hours_" . $stream . " <> 0 ORDER BY `student_ID` asc;", array()); 
+			$studentHoursRS->Execute();
+			foreach($studentHoursRS as $row) {
+				$studentIndex = array_search($row['student_id'],$studentList,true);
+				$studentHours[studentIndex] = $row["hours_" . $stream . "'"];
+			}
+			
+					
+			// Generates array with ith student and jth shift, where j is calculated by day + shift
+			$shiftTotal = 8; //TODONE maybe change to a value that calculated by counting number of columns in db after the stream field or whatever it is
+			$startTime = 9;
+			$preferencesRS = new \PHP\SqlObject("SELECT * FROM preferences  WHERE  `stream` = :stream ORDER BY `user_id` ASC;", array(strtoupper($stream)));
+			$preferencesRS->Execute();
+			$prefArray = array();
+			foreach($preferencesRS as $row) {
+				$studentIndex = array_search($row['user_id'],$studentList,true);
+				for ($i = 0; $i < $shiftTotal; $i++){
+					$prefArray[$studentIndex][$row['day']+$i] = $row["'" . ($i + startTime) % 12 . "'"];
+				}
+			}
+			
+			// iterate over each persons preferences to create objective function
+			$objective = "\* Objective function *\  \n Maximize \n obj:";
+			for ($person = 0; $person < $studentTotal; $person++){
+				for ($shift = 0; $shift < $shiftTotal; $shift++){
+					echo "inside objective";
+					$objective .= " +" . $prefArray[$person][$shift] . " x" . $person . "_" . $shift;
+				}
+			}
+
+			// iterate over each persons to make sure each persons total weekly hours doesn't exceed their specified hours for that stream
+			$constraint = "\n  \* Constraints *\ \n Subject To \n";
+			for ($person = 0; $person < $studentTotal; $person++){
+				$constraint .= "person_" . $person . ":"; 
+				for ($shift = 0; $shift < $shiftTotal; $shift++){
+					$constraint .=  " +x" . $person . "_" . $shift;
+				}
+				//$constraint .= " = " . 2 . "\n"; // TODODONE: after db is changed, changed this line to: 
+				$constraint .= " = " + $studentHours[$person] + "\n"; // decide whether <= or =
+			}
+			
+			// iterate over each shift to make sure each shift has the specified number of people
+			$numAtDesk = array ( 1, 1, 2, 2, 2, 2, 1, 1);
+			for ($shift = 0; $shift < $shiftTotal; $shift++){
+				$constraint .= "shift_" + $shift +":";
+				for ($person = 0; $person < $studentTotal; $person++){
+					$constraint .=  " +x" . $person . "_" . $shift;
+				}
+				$constraint .= " <= " . $numAtDesk[$shift] . "\n";
+			}
+			
+			
+			// TODONE:DONE add int 1, 0 field for each stream called new to db, once added, uncomment the following code 
+			//$newTotal = new \PHP\SqlObject("SELECT COUNT(*) FROM facilitator_shift_preferences WHERE new = :new AND stream = :stream;", array(1, $stream));
+			//$newTotal->Execute();
+			$newTotalRS = new \PHP\SqlObject("SELECT * FROM facilitators JOIN preferences ON facilitators.student_id = preferences.user_id WHERE stream = :stream;", array(strtoupper($stream)));
+			$newTotalRS->Execute();
+			$newTotal = count($newTotalRS);
+			
+			
+			// ensure new plfs are paired with old plfs.If y in newPLFa is true then b 
+			// is effective, otherwise large m will make restraint redundant
+			// Modelled on A - 1 + my < m,  1 - B - my <= 0
+			//$newTotal = 5;  TODO:REMOVE THIS LINE //TODONE delete this once db field new has been added
+			$m = 10000; // arbitrarily large amount
+			$constraintNewA = "";
+			$constraintNewB = "";
+			for ($shift = 0; $shift < $shiftTotal; $shift++){
+				$constraintNewA .= "newPLFa_" . $shift . ": "; 
+				$constraintNewB .= "newPLFb_" . $shift . ": 1- ";
+				
+				for ($person = 0; $person < $newTotal; $person++){
+					$constraintNewA .=  " x" . $person . "_" . $shift;
+				}
+				for ($person = $newTotal; $person < $studentTotal; $person++){
+					$constraintNewB .=  " -x" . $person . "_" . $shift;
+				}
+				$constraintNewA .= " -1 +" . $m . "y" . $shift . " < " . $m . "\n";
+				$constraintNewB .= " -" . $m . "y" . $shift . " <= " . 0 . "\n";
+				
+				$constraint .= $constraintNewA + $constraintNewB;
+			}
+				
+			// Ensures all decision variables are binary (ie less than 1 and integer)
+			$bounds = "\n \* Variable bounds *\ \n Bounds \n";
+			$integers = "\n	\* Integer definitions *\ \n General \n ";	
+			for ($shift = 0; $shift < $shiftTotal; $shift++){
+				for ($person = 0; $person < $studentTotal; $person){
+					$bounds .= " x" . $person . "_" . $shift . " <= 1 \n ";
+					$integers .=" x" . $person . "_" . $shift;
+				}
+				$bounds .= " y" . $shift . " <= 1 \n";
+			}
+
+			// Collect all the strings to generate the input string
+			$input = $objective . $constraint . $bounds . $integers . " End";
+			
+			//TODO: pass input string directly to algorithm, may need to change.
+			echo $input;
+			return $input;
+	}
+
+	function resultsToDatabase(){
+				// Return the javascript array of person, shift and value 
+				if (isset($_POST['variable[]'])) {
+						$results = $_POST['variable[]'];
+						$entries = count($results);
+						$shiftEntries = array( "9-10", "10-11", "11-12", "12-1", "1-2", "2-3", "3-4", "4-5");
+						$dayEntries = array( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+						
+						// assume shift_ID goes from 0 to shiftnum,
+						$sqlEntry = "INSERT INTO autogen_timetable (student_ID, shift_ID, stream, day, shift_time) VALUES";
+						foreach ($results as $entry){
+							$sqlEntry = "( `". array_search($entry[1], $studentArray)  . "`, " . $entry[2] . ", `" . $stream . "`"; 
+							$sqlEntry .= ", `"  . $dayEntries(floor($entry[3] / $shiftTotal)) . "`, `" . $shiftEntries[$entry[3] % $shiftTotal] . "` ), ";
+							///TODONE: the final "," breaks stuff. check if final "," causes sql error and if variable names have spaces at the start
+
+						}
+						
+						// add constructed table to database
+						$automaticTable = new \PHP\SqlObject("$sqlEntry", array());
+						$automaticTable->Execute();
+				}
+	}
+	
+	
 	$stream = "IT"; //TODONE create UI with a button, where this variable gets its value from
 	if(isset($_POST['stream'])) {
 		$stream =  $_POST['stream'];
 		echo "<p>Current Stream: " . $stream . "</p>";
+		generateCPLEX($stream);?>
+		
+		<input type="submit" value= "Generate Table" onclick="run()" />
+		<pre id="log"/>
+	<?php 
+		//resultsToDatabase();
 	} else {
 ?>
 <form name="streamSelect" action="test.php" method="post">
-<input type="radio" name="stream" id="stream-it"value="IT"><label for="stream-it">IT</label><br/>
-<input type="radio" name="stream" id="stream-ma"value="MA"><label for="stream-ma">Maths</label><br/>
-<input type="radio" name="stream" id="stream-sc"value="SC"><label for="stream-sc">Science</label><br/>
-<input type="radio" name="stream" id="stream-dh"value="DH"><label for="stream-dh">Duty Host</label><br/>
+<input type="radio" name="stream" id="stream-it"value="it"><label for="stream-it">IT</label><br/>
+<input type="radio" name="stream" id="stream-ma"value="ma"><label for="stream-ma">Maths</label><br/>
+<input type="radio" name="stream" id="stream-sc"value="sc"><label for="stream-sc">Science</label><br/>
+<input type="radio" name="stream" id="stream-dh"value="dh"><label for="stream-dh">Duty Host</label><br/>
 <input type="submit" value="Select Stream">
 </form>
 <?php
 }
-f
-	// Generate array of studentsID, Index of studentID in array will be used to generate the input CPLEX string
-	$studentListRS = new \PHP\SqlObject("SELECT `student_ID`, `day`, `shift_time` FROM preferences GROUP BY `student_ID ORDER BY `student_ID` ASC WHERE  `stream` = :stream;", array($stream));
-    $studentListRS->Execute();
-	$studentArray = array();
-	$studentTotal = 0;
-	foreach($studentListRS as $row) {
-		$studentArray[$studentTotal] = $row['student_ID'];
-		$studentTotal++;
-	}
-	$studentHours = array(); //TODONE create hours in db and then uncomment code below and edit for loop for generating relevant constraints
-	$studentHoursRS = new \PHP\SqlObject("SELECT `student_ID`, `hours`, FROM facilitator_maxx_hours WHERE `stream` = :stream GROUP BY `student_ID` ORDER BY `student_ID` asc", array($stream));
-    $studentHoursRS->Execute();
-	foreach($studentHoursRS as $row) {
-		$studentIndex = array_search($row['student_ID'],$studentList,true);
-		$studentHours[studentIndex] = $row['hours'];
-	}
-	
-			
-	// Generates array with ith student and jth shift, where j is calculated by day + shift
-	$shiftTotal = 8; //TODONE maybe change to a value that calculated by counting number of columns in db after the stream field or whatever it is
-	$startTime = 9;
-	$preferencesRS = new \PHP\SqlObject("SELECT `student_ID`, `day`, `shift_time` FROM preferences  WHERE  `stream` = :stream;", array($stream));
-    $preferencesRS->Execute();
-	$prefArray = array();
-	foreach($preferencesRS as $row) {
-		$studentIndex = array_search($row['student_ID'],$studentList,true);
-		for ($i = 0; $i < $shiftTotal; $i++){
-			$prefArray[$studentIndex][$row['day']+$i] = $row["'" . ($i + startTime) % 12 . "'"];
-		}
-	}
-
-	// iterate over each persons preferences to create objective function
-	$objective = "\* Objective function *\ *\ \n Maximize \n obj:";
-	for ($person = 0; $person < $studentTotal; $person++){
-		for ($shift = 0; $shift < $shiftTotal; $shift++){
-			$objective .= " +" . $prefArray[$person][$shift] . " x" . $person . "_" . $shift;
-		}
-	}
-
-	// iterate over each persons to make sure each persons total weekly hours doesn't exceed their specified hours for that stream
-	$constraint = "\n  \* Constraints *\ \n Subject To \n";
-	for ($person = 0; $person < $studentTotal; $person++){
-		$constraint .= "person_" . $person . ":"; 
-		for ($shift = 0; $shift < $shiftTotal; $shift++){
-			$constraint .=  " +x" . $person . "_" . $shift;
-		}
-		//$constraint .= " = " . 2 . "\n"; // TODODONE: after db is changed, changed this line to: 
-		$constraint .= " = " + $studentHours[$person] + "\n"; // decide whether <= or =
-	}
-	
-	// iterate over each shift to make sure each shift has the specified number of people
-	$desk = array ( 1, 1, 2, 2, 2, 2, 1, 1);
-	for ($shift = 0; $shift < $shiftTotal; $shift++){
-		$constraint .= "shift_" + $shift +":";
-		for ($person = 0; $person < $studentTotal; $person++){
-			$constraint .=  " +x" . $person . "_" . $shift;
-		}
-		$constraint .= " <= " . $desk[$shift] . "\n";
-	}
-	
-	
-	// TODONE:DONE add int 1, 0 field for each stream called new to db, once added, uncomment the following code 
-	$newTotal = new \PHP\SqlObject("SELECT COUNT(*) FROM facilitator_shift_preferences WHERE new = :new AND stream = :stream;", array(1, $stream));
-    $newTotal->Execute();
-	
-	// ensure new plfs are paired with old plfs.If y in newPLFa is true then b 
-	// is effective, otherwise large m will make restraint redundant
-	// Modelled on A - 1 + my < m,  1 - B - my <= 0
-	//$newTotal = 5;  TODO:REMOVE THIS LINE //TODONE delete this once db field new has been added
-	$m = 10000; // arbitrarily large amount
-	$constraintNewA = "";
-	$constraintNewB = "";
-	for ($shift = 0; $shift < $shiftTotal; $shift++){
-		$constraintNewA .= "newPLFa_" . $shift . ": "; 
-		$constraintNewB .= "newPLFb_" . $shift . ": 1- ";
-		
-		for ($person = 0; $person < $newTotal; $person++){
-			$constraintNewA .=  " x" . $person . "_" . $shift;
-		}
-		for ($person = $newTotal; $person < $studentTotal; $person++){
-			$constraintNewB .=  " -x" . $person . "_" . $shift;
-		}
-		$constraintNewA .= " -1 +" . $m . "y" . $shift . " < " . $m . "\n";
-		$constraintNewB .= " -" . $m . "y" . $shift . " <= " . 0 . "\n";
-		
-		$constraint .= $constraintNewA + $constraintNewB;
-	}
-		
-	// Ensures all decision variables are binary (ie less than 1 and integer)
-	$bounds = "\n \* Variable bounds *\ \n Bounds \n";
-	$integers = "\n	\* Integer definitions *\ \n General \n ";	
-	for ($shift = 0; $shift < $shiftTotal; $shift++){
-		for ($person = 0; $person < $studentTotal; $person){
-			$bounds .= " x" . $person . "_" . $shift . " <= 1 \n ";
-			$integers .=" x" . $person . "_" . $shift;
-		}
-		$bounds .= " y" . $shift . " <= 1 \n";
-	}
-
-	// Collect all the strings to generate the input string
-	$input = $objective . $constraint . $bounds . $integers . " End";
-	
-	//TODO: pass input string directly to algorithm, may need to change.
-	
-	echo $input;
 	//echo "<textarea id='source' cols='50' rows='10' placeholder = 'two' >";
 ?>	
+	<textarea id="source" cols="50" rows="10">
 
-	<textarea id="source" cols="50" rows="10"> 
-\* Objective function *\
+	
+	\* Objective function *\
 Minimize
 obj: +17 x1_1 +23 x2_1 +16 x3_1 +19 x4_1 +18 x5_1 +21 x1_2 +16 x2_2 +20 x3_2 +19 x4_2 +19 x5_2 +22 x1_3 +21 x2_3 +16 x3_3 +22 x4_3 +15 x5_3 +18 x1_4 +16 x2_4 +25 x3_4 +22 x4_4 +15 x5_4 +24 x1_5 +17 x2_5
 +24 x3_5 +20 x4_5 +21 x5_5 +15 x1_6 +16 x2_6 +16 x3_6 +16 x4_6 +25 x5_6 +20 x1_7 +19 x2_7 +17 x3_7 +19 x4_7 +16 x5_7 +18 x1_8 +25 x2_8 +19 x3_8 +17 x4_8 +16 x5_8 +19 x1_9 +18 x2_9 +19 x3_9 +21 x4_9 +23 x5_9
@@ -251,14 +306,11 @@ x2_8 x3_8 x4_8 x5_8 x1_9 x2_9 x3_9 x4_9 x5_9 x1_10 x2_10 x3_10 x4_10 x5_10 x1_11
 x5_14 x1_15 x2_15 x3_15 x4_15 x5_15
 
 End
-
-    </textarea><br>
-    <input type="submit" onclick="run()" />
-    <pre id="log"/>
+</textarea>
 
     <script>
-    var start;
-	var logNode = document.getElementById("log");
+        var start;
+		var logNode = document.getElementById("log");
         var log = glp_print_func = function(value){
             var now = new Date();
 	        var d = (now.getTime() - start.getTime()) / 1000;
@@ -266,15 +318,26 @@ End
             if (d > 60) throw new Error("timeout");
 	        console.log(value);
         };
-		function javascriptToPHP(var  jsvar, var pageURL) { 
-				 $.post(pageURL, {variable: jsVar}); 
-		}
 		
-		function run(){
+		/*function javascriptToPHP( jsvar, pageURL) { 
+				 $.ajax({
+						url: pageURL,
+						type: "POST",
+						data: {
+							'variable[]': jsvar
+						},
+						success: function () {
+						},
+						error: function () {
+						}
+				}
+		}*/
+		
+        function run(){
             start = new Date(); 
 			logNode.innerText = "";
             var lp = glp_create_prob();
-            glp_read_lp_from_string(lp, null, <?php echo $input; ?>); //document.getElementById("source").value);
+            glp_read_lp_from_string(lp, null, document.getElementById("source").value);
 
             glp_scale_prob(lp, GLP_SF_AUTO);
 
@@ -283,7 +346,12 @@ End
 
             var iocp = new IOCP({presolve: GLP_ON});
             glp_intopt(lp, iocp);
-			
+
+            log("obj: " + glp_mip_obj_val(lp));
+            for(var i = 1; i <= glp_get_num_cols(lp); i++){
+                log(glp_get_col_name(lp, i)  + " = " + glp_mip_col_val(lp, i));
+            }
+        
 			var results;
             //l og("obj: " + glp_mip_obj_val(lp));
             for(var i = 1; i <= glp_get_num_cols(lp); i++){
@@ -300,29 +368,7 @@ End
         
     </script>
 
-	<?php
 
-		// Return the javascript array of person, shift and value 
-		$results = $_POST['variable'];
-		$entries = count($results);
-		$shiftEntries = array( "9-10", "10-11", "11-12", "12-1", "1-2", "2-3", "3-4", "4-5");
-		$dayEntries = array( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
-		
-		
-		// assume shift_ID goes from 0 to shiftnum,
-		$sqlEntry = "INSERT INTO autogen_timetable (student_ID, shift_ID, stream, day, shift_time) VALUES";
-		foreach ($results as $entry){
-			$sqlEntry = "( `". array_search($entry[1], $studentArray)  . "`, " . $entry[2] . ", `" . $stream . "`"; 
-			$sqlEntry .= ", `"  . $dayEntries(floor($entry[3] / $shiftTotal)) . "`, `" . $shiftEntries[$entry[3] % $shiftTotal] . "` ), ";
-			///TODONE: the final "," breaks stuff. check if final "," causes sql error and if variable names have spaces at the start
-
-		}
-		
-		// add constructed table to database
-		$automaticTable = new \PHP\SqlObject("$sqlEntry", array());
-		$automaticTable->Execute();
-			
-	?>
 
 
 </body>
