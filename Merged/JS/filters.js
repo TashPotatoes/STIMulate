f9aecf7bb7a0d7a80bad34740aa11566b391017e
anshpr <anthonyshaneprice@gmail.com>
2014-10-08 16:45:00 +1000

    Filters now work, just needs DB data to be pulled in

--- Merged/JS/filters.js
+++ Merged/JS/filters.js
@@ -1,30 +1,17 @@
 // STREAM FILTERS
 $(document).ready(function(){
     Initialise();
+    $(document).tooltip();
 })
 
 
 function Initialise(){
-    $("#filterStreamIt").on("click", function() {
+    $("#filterStreamIt, #filterStreamSc, #filterStreamMa, #filterStreamDh ").on("click", function() {
         UnHideTableElements();
         FilterByStream(this.id);
         ResetSpecialisationFilters();
     });
-    $("#filterStreamSc").on("click", function() {
-        UnHideTableElements();
-        FilterByStream(this.id);
-        ResetSpecialisationFilters();
-    });
-    $("#filterStreamMa").on("click", function() {
-        UnHideTableElements();
-        FilterByStream(this.id);
-        ResetSpecialisationFilters();
-    });
-    $("#filterStreamDh").on("click", function() {
-        UnHideTableElements();
-        FilterByStream(this.id);
-        ResetSpecialisationFilters();
-    });
+
     $("#SpecFilter").on("change", function() {
        // console.log($(this).value.text);
         //get the inputs value
@@ -35,11 +22,12 @@ function Initialise(){
 
 function ResetSpecialisationFilters() {console.log("BLSADS");
     $(".namecard").css("opacity", "1");
+    $(".namecard").parent().css("background-color", "white");
+
 
 }
 function UnHideTableElements() {
     if( $(".specfilter").css("display") ) {
-        console.log('UNHIDE IT');
         $(".specfilter").css("display", "block");
         $(".timetableWrapper").css("display", "block");
         $("#filterSelectMsg").remove();
@@ -51,6 +39,7 @@ function FilterByStream(stream) {
 	$(".namecard").not(StreamClass).css({"visibility": "hidden","display": "none"});
 	$(StreamClass).css({"visibility": "visible", "display": "block"})
 }
+
 
 function FilterBySpecialisations() {
 	/*var Specialisations; 
@@ -68,31 +57,45 @@ function FilterBySpecialisations() {
         	error: UnsuccessfulCall
 	});*/
 		
-	 var Specialisations = [
-		"python",
-		"c++",
-		"c#",
-		"BPM",
-		"teensy"
-	];
-	
-	$("#SpecFilter").autocomplete({
-        	source: Specialisations,
-        	select: function(event, ui) {
-            		FilterSpecialisations();
-        	},
-        	change: function(event, ui) {
-            		FilterSpecialisations();
-        	}
-    	})
+	  var Specialisations = [
+	 	"python",
+	 	"BPM",
+	 	"teensy",
+        "c++",
+        "c#"
+	 ];
 
-}
 
-function FilterSpecialisations() {
-    var specialisation = $("#SpecFilter").val();
-    console.log(specialisation);
-    if (specialisation == 'python') {
-        $(".n8571091").css("opacity", "1");
-        $(".namecard").not(".n8571091").css("opacity", ".4");
-    };
+    var fac_spec_matrix = [
+        ['python','n8571091', 'n1000001'],
+        ['BPM','n8571091', 'n1000014', 'n1000009' ],
+        ['teensy', 'n1000021','n1000022'],
+        ['C++', 'n1000014', 'n1000009' ],
+        ['C#', 'n1000014', 'n1000011' ]
+    ];
+
+
+    $("#SpecFilter").autocomplete({
+        source: Specialisations,
+        search: function( event, ui ) {},
+        close: function( event, ui ) {}
+    });
+    $("#SpecFilter").on("autocompleteclose", function() { 
+        var specIndex = $.inArray(this.value, Specialisations);
+        var test = $.inArray(this.value, fac_spec_matrix);
+        console.log(fac_spec_matrix);
+        console.log(test);
+        if (specIndex != -1) {
+            ResetSpecialisationFilters();
+            $.each(fac_spec_matrix[specIndex], function( index, value ) {
+                $(".namecard").css("opacity", ".4");
+                if(index > 0) {
+                    value = "." + value;
+                    $(value).not(".absent").parent().css("background-color", "rgb(240,240,255)");
+                    $(value).not(".absent").css("opacity", "1");
+               }
+            });
+        }
+    });
+
 }
