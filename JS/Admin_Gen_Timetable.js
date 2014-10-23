e8d2c8b61edf8878d217f4137cb204782693df13
Natasha <natasha.bethany.moffat@gmail.com>
2014-10-23 15:43:18 +1000

    Debugging Simplex migration - posting works

--- JS/Admin_Gen_Timetable.js
+++ JS/Admin_Gen_Timetable.js
@@ -48,7 +48,7 @@
 						results.push([person, shift, value]); //results[person][shift]  = value; 
 					}
 					console.log(results);
-					//javascriptToPHP(results, '../Include/Admin_Gen_Timetable.inc');
+					//javascriptToPHP(results, '../Admin_Gen_Timetable.php');
 				
 				} else {
 					alert("There was no feasible solution");
