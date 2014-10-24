46bd72913ef0b0938edebd04449235857371d60c
Natasha <natasha.bethany.moffat@gmail.com>
2014-10-24 20:47:03 +1000

    Nearly finished pearls stuff, still generating sql

--- JS/ColourChange.js
+++ JS/ColourChange.js
@@ -8,14 +8,14 @@ function clickColorEvent() {
 		if ($(this).hasClass("GREEN")) {
 			$(this).removeClass("GREEN");
 			$(this).addClass("YELLOW");
-			$(this).css({'background-color': 'rgb(75,75,230)'});
+			$(this).css({'background-color': '#438EB9'});
 			$(this).html("2nd");
 
 		} else if ($(this).hasClass("YELLOW")) {
 			$(this).removeClass("YELLOW");
 			$(this).addClass("RED");
-			$(this).css({'background-color': 'rgb(50,50,190)'});
-			$(this).html("3rd");
+			$(this).css({'background-color': '#28556F'});
+			$(this).html("1st");
 		} else if ($(this).hasClass("RED")) {
 			$(this).removeClass("RED");
 			$(this).addClass("WHITE");
@@ -24,8 +24,8 @@ function clickColorEvent() {
 		} else if ($(this).hasClass("WHITE")){
 			$(this).removeClass("WHITE");
 			$(this).addClass("GREEN");
-			$(this).css({'background-color': 'rgb(50,50,1900)'});
-			$(this).html("1st");
+			$(this).css({'background-color': '#8EBBD5'});
+			$(this).html("3rd");
 
 		} 
 	});
