1e5d541f790a7c2bd2f14bb8450ffd48828d2331
Hayden Jackson <crazygravy89@hotmail.com>
2014-10-27 01:29:53 +1000

    Finished Preferences.

--- JS/ColourChange.js
+++ JS/ColourChange.js
@@ -5,37 +5,34 @@ $(document).ready(function(){
 /* Function to visually alter the volunteer hours data input */
 function clickColorEvent() {
 	$('td').on('click', function(){
+
 		if ($(this).hasClass("GREEN")) {
 			$(this).removeClass("GREEN");
 			$(this).addClass("YELLOW");
 			$(this).css({'background-color': '#438EB9'});
-			$(this).html("2nd");
-			$(this).childen(".hiddeninput").value() = 9;
-			//$(this).find("input[type='hidden']").val(9);
+			$(this).children('p').html("2nd");
+			$(this).children(".hiddeninput").val(9);
 
 		} else if ($(this).hasClass("YELLOW")) {
 			$(this).removeClass("YELLOW");
 			$(this).addClass("RED");
 			$(this).css({'background-color': '#28556F'});
-			$(this).html("1st");
-			//$(this).find("input[type='hidden']").val(27);
-			$(this).childen(".hiddeninput").value() = 27;
+			$(this).children('p').html("1st");
+			$(this).children(".hiddeninput").val(27);
 
 		} else if ($(this).hasClass("RED")) {
 			$(this).removeClass("RED");
 			$(this).addClass("WHITE");
 			$(this).css({'background-color': 'rgb(255,255,255)'});
-			$(this).html("");
-			//$(this).find("input[type='hidden']").val(-50);
-			$(this).childen(".hiddeninput").value() = -50;
-			
+			$(this).children('p').html(" ");
+			$(this).children(".hiddeninput").val(-50);
+
 		} else if ($(this).hasClass("WHITE")){
 			$(this).removeClass("WHITE");
 			$(this).addClass("GREEN");
 			$(this).css({'background-color': '#8EBBD5'});
-			$(this).html("3rd");
-			//$(this).find("input[type='hidden']").val(3);
-			$(this).childen(".hiddeninput").value() = 3;
+			$(this).children('p').html("3rd");
+			$(this).children(".hiddeninput").val(3);
 		} 
 	});
 }
